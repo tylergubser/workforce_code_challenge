08f4757c1d5a23fbd3a918e200af6e036a5e2f7d
tylergubser <tylergubser2200@gmail.com>
2022-06-05 08:08:08 -0500

    added current user to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+    helper_method :current_user,
+
+    def current_user
+        if(session[:user_id] && session[:user_id] != nil)
+            return User.find(session[:user_id])
+        else
+            nil
+        end
+    end
 end
