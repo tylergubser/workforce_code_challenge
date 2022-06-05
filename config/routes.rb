f52b604e012c342a8cee7d94df5358a490e0dbc4
tylergubser <tylergubser2200@gmail.com>
2022-06-05 18:57:04 -0500

    updated sessions view

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,13 @@ Rails.application.routes.draw do
   resources :organisations
   resources :shifts
   resources :users
-  get '/', to: 'sessions#index', as: 'root'
-  get '/login', to: 'sessions#new', as: 'login'
-  post '/', to: 'sessions#create'
+  root "sessions#home"
+  # get '/', to: 'sessions#index', as: 'root'
+  # get '/login', to: 'sessions#new', as: 'login'
+  get '/login', to: 'sessions#login'
+  post '/login', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
+  post '/logout', to: 'sessions#destroy'
   get '/signup', to: 'users#new', as:'signup'
   post '/signup', to: 'users#create'
 end
