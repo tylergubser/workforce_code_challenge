4a0a1c7daab490cc46abdb01102d21cd523695ca
tylergubser <tylergubser2200@gmail.com>
2022-06-14 09:39:08 -0500

    missing validation

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -1,6 +1,6 @@
 class Shift < ApplicationRecord
     belongs_to :user
-
+    validates :user_id, presence: true
     validates :start, presence: true
     validates :finish, presence: true
     validates :break_length, presence: true
